96e854e25ad74121589e810f2e78a764d600be2f
Justin13131 <89062313+Justin13131@users.noreply.github.com>
2021-08-20 15:04:04 +0800

    add files

---
+++ BasicsAndPrinting/ALetterToYourself/ALetterToYourself.java
@@ -0,0 +1,18 @@
+public class ALetterToYourself
+{
+  public static void main (String [] args)
+  {
+    System.out.println("+-------------------------------------------------------------------+");
+    System.out.println("|                                                                   |");
+    System.out.println("|                                                                   |");
+    System.out.println("|                                                                   |");
+    System.out.println("|                                                                   |");
+    System.out.println("|                                                                   |");
+    System.out.println("|                                                                   |");
+    System.out.println("|                                     Justin Hsu                    |");
+    System.out.println("|                                     Pacific American School       |");
+    System.out.println("|                                     justinhsu@pacificamerican.org |");
+    System.out.println("|                                                                   |");
+    System.out.println("+-------------------------------------------------------------------+");
+  }
+}
---
+++ BasicsAndPrinting/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials
+{
+  public static void main (String [] args)
+  {
+    System.out.println("JJJJJ     OOOOO    RRRR     GGG    EEEEE         BBBB      A      L         DDDD     EEEEE    RRRR       A      M   M      A  ");
+    System.out.println("  J      O     O   R   R    G   G  E             B   B    A A     L         D   D    E        R   R     A A     MM MM     A A ");
+    System.out.println("  J      O     O   R   R    G      E             B   B   A   A    L         D   D    E        R   R    A   A    MM MM    A   A");
+    System.out.println("  J      O     O   RRRR     G GGG  EEE           BBBB    AAAAA    L         D   D    EEE      RRRR     AAAAA    M M M    AAAAA");
+    System.out.println("J J      O     O   R R      G   G  E             B   B   A   A    L         D   D    E        R R      A   A    M   M    A   A");
+    System.out.println("J J      O     O   R  R     G   G  E             B   B   A   A    L         D   D    E        R  R     A   A    M   M    A   A");
+    System.out.println(" J        OOOOO    R   R     GG    EEEEE         BBBB    A   A    LLLLLL    DDDD     EEEEE    R   R    A   A    M   M    A   A");
+    }
+  }
