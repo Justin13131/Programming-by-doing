8bec204ce95f005fb0470b918f01fcc16ae31385
Justin13131 <89062313+Justin13131@users.noreply.github.com>
2021-08-31 09:33:49 +0800

    add file

--- Variables/VariablesAndNames/VariablesAndNames.java
+++ Variables/VariablesAndNames/VariablesAndNames.java
@@ -6,12 +6,12 @@ public class VariablesAndNames
     int cars, drivers, passengers, cars_not_driven, cars_driven;
     double space_in_a_car, carpool_capacity, average_passengers_per_car;
 
-    cars = 100;
-    space_in_a_car = 4.0;
-    drivers = 30;
-    passengers = 90;
-    cars_not_driven = cars - drivers;
-    cars_driven = drivers;
+    cars = 100; //Number of cars
+    space_in_a_car = 4.0; //Space in car
+    drivers = 30; //Number of drivers
+    passengers = 90; //Passengers
+    cars_not_driven = cars - drivers; //Cars not driven
+    cars_driven = drivers; //Cars driven
     carpool_capacity = cars_driven * space_in_a_car;
     average_passengers_per_car = passengers/cars_driven;
     //These are the variables
