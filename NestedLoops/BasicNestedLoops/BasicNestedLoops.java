750648d8f9208b738f19c8b05680c9f51b622d0b
Justin13131 <89062313+Justin13131@users.noreply.github.com>
2021-10-12 13:57:48 +0800

    add files

---
+++ NestedLoops/BasicNestedLoops/BasicNestedLoops.java
@@ -0,0 +1,13 @@
+//NastedLoops BasicNasedLoops 50pts Justin Hsu
+public class BasicNestedLoops {
+    
+    public static void main(String[] args){
+        for(int i =0; i<=5; i++){
+            System.out.println();
+            for(int j =0; j<=5; j++){
+                System.out.print("("+i+","+j+")  ");
+                
+            }
+        }
+    }
+}
