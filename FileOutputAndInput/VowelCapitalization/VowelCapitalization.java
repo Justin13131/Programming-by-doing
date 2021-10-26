bb815bbb514c7ab3bd46e8da976e17adaa53ac4d
Justin13131 <89062313+Justin13131@users.noreply.github.com>
2021-10-26 14:44:14 +0800

    add files

--- FileOutputAndInput/VowelCapitalization/VowelCapitalization.java
+++ FileOutputAndInput/VowelCapitalization/VowelCapitalization.java
@@ -1,3 +1,35 @@
+//FileOutputAndInput VowelCapitalization 80pts Justin Hsu
+import java.util.*;
+import java.io.*;
+
+public class VowelCapitalization{
+	public static void main(String[] args) throws Exception{
+		Scanner keyboard = new Scanner(System.in);
+        //Ask
+		System.out.print("Open which file: ");
+        //Input
+		String file = keyboard.next();
+		System.out.println();
+		
+        //File
+		File in = new File(file);
+		Scanner input = new Scanner(in);
+		//While File Exists
+		while ( input.hasNext() )
+		{
+			String line = input.nextLine();
+			//How Many Lines
+			for ( int i = 0; i < line.length(); i++ )
+			{
+                //Characters
+				char c = line.charAt(i);
+				
+				if ( c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' )
+				{
+                    //Make it upperCase
+					System.out.print( Character.toUpperCase(c) );
+				}
+				else
                     //Print C
 					System.out.print( c );
 			}	
