7dc8c828ac99f6566a15afc5efa8276a59862abc
Justin13131 <89062313+Justin13131@users.noreply.github.com>
2022-01-14 14:26:10 +0800

    add files

--- C++/KeyboardInput/BMICalculator/main.cpp
+++ C++/KeyboardInput/BMICalculator/main.cpp
@@ -6,7 +6,7 @@ int main(){
     int weight = 0;
     int BMI = 0;
 
-    cout << "Enter your height and inches: ";
+    cout << "Enter your height in inches: ";
     cin >> height;
     cout << "Enter your weight in pounds: ";
     cin >> weight;
@@ -14,15 +14,7 @@ int main(){
     BMI = (weight*703)/(height*height);
 
     cout << "Your BMI is: " << BMI << endl << endl;
-    cout << "How to read your BMI: " << endl;
-    cout << "Below 18.5 : Underweight" << endl;
-    cout << "18.5 - 24.9 : Average " << endl;
-    cout << "25.0 - 29.9 : Overweight" << endl;
-    cout << "30 and above : Obese" << endl;
 
     system("pause"); 
-
-
-
 
 }
