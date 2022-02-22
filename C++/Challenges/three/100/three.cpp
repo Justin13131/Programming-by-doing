1eb4b00249cc26254237282027318497053359b6
Justin13131 <89062313+Justin13131@users.noreply.github.com>
2022-02-22 13:27:16 +0800

    add

--- C++/Challenges/three/100/three.cpp
+++ C++/Challenges/three/100/three.cpp
@@ -1,34 +1,3 @@
-// #include <iostream>
-// #include <stdio.h>
-// #include <math.h>
-// #include <string>  
-
-// using namespace std;
-// int result = 0, x;
-
-// int main() {
-//     cout << "Power: ";
-//     cin >> x;
-//     for (long long i = 2; i < 355000000; i++) { 
-//         int sumOfPowers = 0; int number = i; 
-//         while (number > 0) {
-//             int d = number % 10;
-//             number /= 10;
- 
-//             int temp = d;
-//             //power x (input)
-//             for(int j = 1; j < x; j++){
-//                 temp *= d;
-//             }
-//             sumOfPowers += temp;
-//         }
- 
-//         if (sumOfPowers == i) {
-//             result += i;
-//         }
-//     }
-//     cout << result;
-// }
 #include <iostream>
 #include <string>
 using namespace std;
