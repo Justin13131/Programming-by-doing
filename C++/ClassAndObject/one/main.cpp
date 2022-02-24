529f562a5244ed8ae464fd3f2fde22d14b63590b
Justin13131 <89062313+Justin13131@users.noreply.github.com>
2022-02-24 14:38:56 +0800

    add files

--- C++/ClassAndObject/one/main.cpp
+++ C++/ClassAndObject/one/main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
+#include <list>
 using namespace std; 
-class Dog {
+class doggo {
     public:
         int a;
         double w;
+        string name;
+        string ownerName;
+        list<string>favoriteFoods;
+
+        doggo(string cName, string cOwner, int cAge, double cW){
+            name = cName;
+            ownerName = cOwner;
+            a = cAge;
+            w = cW;
+
+        }
+
+        void GetInfo(){
+            cout << "Name:= " << name << endl;
+            cout << "Age:= " << a << endl;
+            cout << "ownerName:= " << ownerName << endl;
+            cout << "weight:= " << w << endl;
+        }
 };
-
 int main() {
-    Dog doggo;
+    doggo doggo1("Nico", "pp",2,3);
+    
+    doggo1.GetInfo();
 
-    doggo.a = 3;
-    doggo.w = 4.5;
-
-    cout << "Doggo's age: " << doggo.a << endl;
-
-    cout << "Doggo's weight: " << doggo.w << endl;
 }
