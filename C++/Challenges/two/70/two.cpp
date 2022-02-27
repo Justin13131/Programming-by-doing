02fe6fb284305a518d162504e97f7a3155aba1b5
Justin13131 <89062313+Justin13131@users.noreply.github.com>
2022-02-27 22:52:36 +0800

    add files

--- C++/Challenges/two/70/two.cpp
+++ C++/Challenges/two/70/two.cpp
@@ -1,56 +1,125 @@
-#include <iostream>  
+#include <iostream>
 #include <math.h>
-#include <stdio.h>
-
-
+#include <vector>
+#include "string.h"
 using namespace std;
- 
-int* GetBigInteger(string str);
 
+void Pow(int i,vector<int>&pow_ans);
+void Multi(vector<int> na, vector<int>nb,vector<int>&ans);
+vector<int> Add(vector<int> a, vector<int> b);
 int main() {
-    cout<<"test";
-    string str = "0";
+vector<int> ans;
+for(int i=1;i<=1000;++i)
+  {
+    vector<int> pow_ans;
+    Pow(i,pow_ans);
+    ans = Add(ans,pow_ans);
+  }
+    for(int i=ans.size()-1;i>=0;--i)
+      {
+        cout<<ans[i];
+      }
+    cout<<endl;
+  
+}
+void Pow(int i,vector<int> & pow_ans){
+    vector<int>na;
+    vector<int>nb;
+    string a_reverse = to_string(i);
+    auto len = a_reverse.length();
+    for(int j=0;j<len/2;j++){
+    swap(a_reverse[j], a_reverse[len-1-j]);
+    }
+    for(int j=0;j<len;++j)
+    {
+      na.push_back(a_reverse[j]-'0');
+      nb.push_back(a_reverse[j]-'0');
+    }
+  if(i>1)
+  {
+    for(int j=1;j<i;++j)
+      {
+        for(int k=0;k<pow_ans.size();++k)
+          {
+            pow_ans[k]=0;
+          }
+        
+    
+      Multi(na,nb,pow_ans);
+        na = pow_ans;
+        
+      }
+    }
+  else
+  {
+    pow_ans.push_back(1);
+  }
 
-    for (int i = 1; i <= 1000; i++) {
-        string str1 = "0";
-        for (int j = 1; j < i; j++) {
-            str1 = i;
-        }
-        str += str1;
-    }
-    int* arr = GetBigInteger(str);
-    for (int i = 0; i < str.size(); i++) {
-        cout <<arr[i];
-    }
-    return 0;
 }
 
-// array consisting integers individually
-int* GetBigInteger(string str)
+void Multi(vector<int> na, vector<int>nb,vector<int>&pow_ans){
+
+  for(int k=0;k<na.size();++k)
+        {
+          for(int l=0;l<nb.size();++l)
+            {
+              if(pow_ans.size()<=(k+l))
+              {
+                  pow_ans.push_back(na[k] * nb[l]);                
+              }
+              else
+              {
+                pow_ans[k+l] += na[k] * nb[l];
+              }
+              
+                if(pow_ans[k+l]>=10){
+                  if(pow_ans.size()<=(k+l+1))
+                  {
+                               pow_ans.push_back(pow_ans[k+l]/10); 
+                  }
+                  else
+                  {
+            pow_ans[k+l+1] += pow_ans[k+l]/10;
+                    }
+            pow_ans[k+l] = pow_ans[k+l]%10;
+            }
+        }
+        }
+}
+
+vector<int> Add(vector<int> na, vector<int> nb){
+
+  if(na.size()<nb.size())
 {
-    int x = str.size(), a = 0;
- 
-    // Create an array to store the big
-    // integer into it.
- 
-    // Make the array size same as the
-    // size of string str
-    int* arr = new int[str.size()];
- 
-    // Loop to extract string elements
-    // into the array one by one
-    while (a != x) {
- 
-        // Subtracting '0' to convert
-        // each character into digit
- 
-        // str[a] - '0'
-        // = ASCII(str[a]) - ASCII('0')
-        // = ASCII(str[a] - 48
-        arr[a] = str[a] - '0';
-        a++;
+  int diff = nb.size()-na.size();
+  for(int i=0;i<diff;++i)
+    {
+      na.push_back(0);
     }
- 
-    // Return the reference of the array
-    return arr;
+}
+
+  if(na.size()>nb.size())
+{
+  int diff = na.size()-nb.size();
+  for(int i=0;i<diff;++i)
+    {
+      nb.push_back(0);
+    }
+}
+  
+  vector<int>sum;
+  int carry = 0;
+
+  
+for(int i=0;i<na.size();i++){
+     sum.push_back(na[i]+nb[i]+carry);
+  
+    if(sum[i]>=10){
+        sum[i]-=10;
+        carry=1;
+    }
+    else carry=0;
+}
+
+  return sum;
 }
