af91525084e106d53cccd609c5bee0385647e13c
Justin13131 <89062313+Justin13131@users.noreply.github.com>
2022-03-01 12:29:22 +0800

    add files

---
+++ C++/ClassAndObject/three/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+#include <math.h>
+#include <stdio.h>
+
+using namespace std;
+
+
+class Student {
+    public: 
+        string Name;
+        int age;
+        int gradelevel;
+        double GPA; //average % of grades * 5
+        char gender; //M or F
+        double grades[4]; //[0] is English, [1] is Math, [2] is Science, [3] is Social Studies]
+    Student( string n, int a, int g, char gen){
+
+    }
+    calcGPA() {
+
+    }
+    setGrade(string course, double grade) {
+
+    }
+    printGrades() //print out the users individual grades and GPA {
+
+    }
+    getGPA() {
+
+    }
+    getGrade(string course) {
+        
+    }
+
+    
+};
+
+
+int main() {
+
+}
