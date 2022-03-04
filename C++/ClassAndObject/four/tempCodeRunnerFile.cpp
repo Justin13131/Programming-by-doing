2b5e14e40c02ef635aa8be53f1307bf48b3d1691
Justin13131 <89062313+Justin13131@users.noreply.github.com>
2022-03-04 15:00:31 +0800

    add files

---
+++ C++/ClassAndObject/four/tempCodeRunnerFile.cpp
@@ -0,0 +1,20 @@
+ // string res2;
+    // int i4;
+    // int which;
+    // int which2;
+    // while( i4 < 1) {
+    //     cout << "Anymore distances to find? (y or n) ";
+    //     cin >> res2;
+    //     if (res2 == ("y")) {
+    //         cout << endl << "Mercury 0, Venus 1, Earth 2, Mars 3, Jupiter 4, Saturn 5, Uranus 6, Neptune 7" << endl << endl;
+    //         cout << "Planet 1: ";
+    //         cin >> which;
+    //         cout << "Planet 2: ";
+    //         cin >> which2;
+    //         cout << endl;
+    //         cout << "Distances between this two planets: " << distanceP(solarSystem[which], solarSystem[which2]) << endl;
+    //     }else{
+    //         cout << "Okay, bye." << endl;
+    //         i4++;
+    //     }
+    // }
