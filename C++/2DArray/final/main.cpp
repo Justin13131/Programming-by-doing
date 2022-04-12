bacdcbf761f13485d12bff0708d5fe81b21e24b9
Justin13131 <89062313+Justin13131@users.noreply.github.com>
2022-04-12 15:17:53 +0800

    add files

---
+++ C++/2DArray/final/main.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int h, v;
+int test = 0;
+vector <char> hori = {};
+
+void reverse(){
+    int pri = 0;
+    int ans = v*h;
+        cout << endl;
+        char mat[v][h];
+        cout << "sort" << endl;
+        for(int i = 0; i < ans; i++){
+            cout << hori.at(pri) << " ";
+            pri++;
+        }
+        cout << endl;
+        
+        for(int i = 0 ; i < v ; i++)  {  
+            if(i%2==0)  {  
+                for(int j = h-1 ; j>=0 ; j--){
+                    mat[i][j] = hori.at(pri);
+                    pri++; 
+                }
+            }  else  {   
+                for(int j = 0 ; j < h ; j++){
+                    mat[i][j] = hori.at(pri); 
+                    pri++;
+                }      
+            }  
+        }  
+        pri = 0;
+        for (int i = 0; i < h / 2; i++) {
+            for (int j = i; j < h - i - 1; j++) {
+                // Swap elements of each cycle
+                // in clockwise direction
+                char temp = mat[i][j];
+                mat[i][j] = mat[h - 1 - j][i];
+                mat[h - 1 - j][i] = mat[h - 1 - i][h - 1 - j];
+                mat[h - 1 - i][h - 1 - j] = mat[j][h - 1 - i];
+                mat[j][h - 1 - i] = temp;
+            }
+        }
+        
+        
+        
+        for(int i = 0; i < v; i++){
+            for(int j = 0; j < h; j++){
+                cout << mat[i][j] << " ";
+            }
+            cout<<endl;
+        }
+}
+int main(){
+    do{
+        srand(time(NULL));
+        cout << "Size(Horizontal): "; 
+        cin >> h;
+        cout << endl << "Size(Vertical): ";
+        cin >> v;
+        char c;
+        int r;
+        int ans = v*h;
+        if(h == 0){
+            if(v==0){
+                break;
+            }
+        }
+        for(int i = 0; i < ans; i++){
+            r = rand() % 26;   
+            c = 'a' + r;
+            hori.push_back(c);
+        }
+        int pri = 0;
+        cout << "Before: " << endl;
+        for(int i = 0; i < v; i++){
+            for(int j = 0; j < h; j++){
+                cout << hori.at(pri) << " ";
+                pri++;
+            }
+            cout << endl;
+        }
+        sort(hori.begin(), hori.end());
+
+        pri = 0;
+        cout << endl;
+        char mat[v][h];
+        cout << "sort" << endl;
+        for(int i = 0; i < ans; i++){
+            cout << hori.at(pri) << " ";
+            pri++;
+        }
+        cout << endl;
+        
+        cout << "After: " << endl;
+        for(int i = 0 ; i < v ; i++)  {  
+            if(i%2==0)  {  
+                for(int j = 0 ; j < h ; j++){
+                    mat[i][j] = hori.at(pri);
+                    pri++; 
+                }
+            }  else  {   
+                for(int j = h-1 ; j>=0 ; j--) {
+                    mat[i][j] = hori.at(pri); 
+                    pri++;
+                }      
+            }  
+        }  
+        cout << test;
+        pri = 0;
+        for (int i = 0; i < h / 2; i++) {
+            for (int j = i; j < h - i - 1; j++) {
+                // Swap elements of each cycle
+                // in clockwise direction
+                char temp = mat[i][j];
+                mat[i][j] = mat[h - 1 - j][i];
+                mat[h - 1 - j][i] = mat[h - 1 - i][h - 1 - j];
+                mat[h - 1 - i][h - 1 - j] = mat[j][h - 1 - i];
+                mat[j][h - 1 - i] = temp;
+            }
+        }
+        
+        for(int i = 0; i < v; i++){
+            for(int j = 0; j < h; j++){
+                cout << mat[i][j] << " ";
+            }
+            cout<<endl;
+        }
+        
+        reverse();
+        
+        hori.clear();
+        cout << endl;
+
+    }while(test == 0);
+    cout << endl << "Bye";
+	return 0;
+    
+
+}
+
+ 
