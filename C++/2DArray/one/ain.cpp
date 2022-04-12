c8fdf4d269f43d6bf28cd5941525d971ab1c0e23
Justin13131 <89062313+Justin13131@users.noreply.github.com>
2022-04-12 14:42:46 +0800

    add files

--- C++/2DArray/one/ain.cpp
+++ C++/2DArray/one/ain.cpp
@@ -1,58 +1,84 @@
 #include <iostream>
-#include <stdlib.h>
-#include <ctime>
 #include <vector>
-using namespace std;
-int h, v;
+#include <array>
+#include <ctime>
+#include <random>
+#include <bits/stdc++.h>
 
-int main(){
-    srand(time(NULL));
-    cout << "Size(Horizontal): "; 
-    cin >> h;
-    cout << endl << "Size(Vertical): ";
-    cin >> v;
-    char c;
-    int r;
-    vector <char> hori = {};
-    for(int i = 0; i < v; i++){
-        for(int j = 0; j < h; j++){
-            r = rand() % 26;   
-            c = 'a' + r;
-            hori.push_back(c);
+
+template<typename Matrix2D>
+void print_matrix(Matrix2D const & vec)
+{
+    std::cout << "size of matrix is [" << vec[0].size() << " x " << vec.size() << "]\n";
+    for (auto const & inner_vec : vec) {
+        for (auto const & item : inner_vec) {
+            std::cout << item << ", ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+template<typename OriginalMatrix2D, typename RotatedMatrix2D>
+void rotate_2d_matrix_clockwise_impl(OriginalMatrix2D const & matrix,
+                                     RotatedMatrix2D        & rotated_matrix,
+                                     int              const M,
+                                     int              const N)
+{
+    for (auto x = 0; x < N; ++x) {
+        for (auto y = 0; y < M; ++y) {
+            // Source : https://stackoverflow.com/questions/4780119/2d-euclidean-vector-rotations
+            rotated_matrix[y][-x -1 +N] = matrix[x][y];
         }
     }
-    int num = 0;
-    char matrix[v][h] = {};
-    for(int i = 0; i < v; i++){
-        for(int j = 0; j < h; j++){
-            matrix[i][j] = hori.at(num);
-            num++;
+}
 
-        }
+template<typename T, size_t M, size_t N>
+auto rotate_2d_matrix_clockwise(std::array<std::array<T, M>, N> const & original_matrix)
+    -> std::array<std::array<T, N>, M>
+{
+    std::array<std::array<T, N>, M> rotated_matrix;
+    rotate_2d_matrix_clockwise_impl(original_matrix, rotated_matrix, M, N);
+    return rotated_matrix;
+}
+
+template<typename Matrix2D>
+auto rotate_2d_matrix_clockwise(Matrix2D const & original_matrix) -> Matrix2D
+{
+    int const M = original_matrix[0].size();
+    int const N = original_matrix.size();
+    Matrix2D rotated_matrix;
+    rotated_matrix.resize(M);
+    for (auto x = 0; x < M; ++x) {
+        rotated_matrix[x].resize(N);
     }
-    cout << "Before: " << endl;
-     for(int i = 0; i < v; i++){
-        for(int j = 0; j < h; j++){
-            cout << matrix[i][j] << " ";
+    rotate_2d_matrix_clockwise_impl(original_matrix, rotated_matrix, M, N);
+    return rotated_matrix;
+}
+
+int main()
+{   
+    int n, m;
+
+    int ct = 1;
+    do{
+        std::cout << "Size(Horizontal): "; 
+        std::cin >> n;
+        std::cout << std::endl << "Size(Vertical): ";
+        std::cin >> m;
+        if(n == 0){
+            if(m==0){
+                break;
+            }
         }
-        cout<<endl << "";
-    }
-    vector<vector<char>> vec(matrix);
+        srand(time(NULL));
+        std::vector<std::vector<char>> data2 ( n , std::vector<char> (m, 0));
+        generate(data2.begin(), data2.end(), rand);
+        std::cout << "Before Rotation :\n";
+        print_matrix(data2);
+        std::cout << "\nAfter Clockwise Rotation :\n";
+        print_matrix(rotate_2d_matrix_clockwise(data2));
 
-
+    }while(ct== 1);
     
-    cout << "After: " << endl;
-    for(int i = 0; i < v; i++){
-        for(int j = 0; j < h; j++){
-            cout << matrix[i][j] << " ";
-        }
-        cout<<endl << "";
-    }
-
-
-	cout << endl;
-
-	return 0;
-    
-
+    return 0;
 }
--- C++/2DArray/test/one.cpp
+++ C++/2DArray/test/one.cpp
@@ -5,93 +5,157 @@
 #include <algorithm>
 using namespace std;
 int h, v;
-
-int main(){
-    srand(time(NULL));
-    cout << "Size(Horizontal): "; 
-    cin >> h;
-    cout << endl << "Size(Vertical): ";
-    cin >> v;
-    char c;
-    int r;
-    int ans = v*h;
-    vector <char> hori = {};
-    for(int i = 0; i < ans; i++){
-        r = rand() % 26;   
-        c = 'a' + r;
-        hori.push_back(c);
-    }
+int test = 0;
+vector <char> hori = {};
+void reverse(){
+    char matrev[v][h];
     int pri = 0;
-    cout << "Before: " << endl;
-    for(int i = 0; i < v; i++){
-        for(int j = 0; j < h; j++){
+    sort(hori.begin(), hori.end(),greater<char>());
+    cout << endl << "Reverse: ";
+        cout << endl;
+        pri = 0;
+        int count = v;
+        int size = hori.size();
+        int i;
+        int ch;
+        for(i = 0; i < h+1; i++){
+            for(int j = 0; j < v; j++){
+                if(size == 0){
+                    for(int k = 0; k <= ch; k++){
+                        matrev[j][i] = hori.at(pri);
+                    }
+                    break;
+                }else{
+                    matrev[j][i] = hori.at(pri);
+                    pri++; 
+                    size--;
+                }
+            }
+            pri--;
+            ch++;
+            i++;
+            for(int j = 0; j <= v; j++){
+                if(size == 0){
+                    for(int k = 0; k <= ch; k++){
+                        matrev[count][i] = hori.at(pri);
+                        
+                    }
+                    break;
+                }else{
+                    matrev[count][i] = hori.at(pri);
+                    pri++;
+                    count--; 
+                    size--;
+                }
+                
+            }
+            count = v;
+        }
+
+
+        
+        for(int i = 0; i < v; i++){
+            for(int j = 0; j < h; j++){
+                cout << matrev[i][j] << " ";
+            }
+            cout<<endl;
+        }
+}
+int main(){
+    do{
+        srand(time(NULL));
+        cout << "Size(Horizontal): "; 
+        cin >> h;
+        cout << endl << "Size(Vertical): ";
+        cin >> v;
+        char c;
+        int r;
+        int ans = v*h;
+        if(h == 0){
+            if(v==0){
+                break;
+            }
+        }
+        for(int i = 0; i < ans; i++){
+            r = rand() % 26;   
+            c = 'a' + r;
+            hori.push_back(c);
+        }
+        int pri = 0;
+        cout << "Before: " << endl;
+        for(int i = 0; i < v; i++){
+            for(int j = 0; j < h; j++){
+                cout << hori.at(pri) << " ";
+                pri++;
+            }
+            cout << endl;
+        }
+        sort(hori.begin(), hori.end());
+
+        pri = 0;
+        cout << endl;
+        char mat[v][h];
+        cout << "sort" << endl;
+        for(int i = 0; i < ans; i++){
             cout << hori.at(pri) << " ";
             pri++;
         }
         cout << endl;
-    }
-    sort(hori.begin(), hori.end());
-
-    pri = 0;
-    cout << endl;
-    char mat[v][h];
-    cout << "sort" << endl;
-    for(int i = 0; i < ans; i++){
-        cout << hori.at(pri) << " ";
-        pri++;
-    }
-    cout << endl;
-    pri = 0;
-    int count = v;
-    int size = hori.size();
-    int i;
-    int ch;
-    cout << "After: " << endl;
-    for(i = 0; i < h+1; i++){
-        for(int j = 0; j < v; j++){
-            if(size == 0){
-                for(int k = 0; k <= ch; k++){
+        pri = 0;
+        int count = v;
+        int size = hori.size();
+        int i;
+        int ch;
+        cout << "After: " << endl;
+        for(i = 0; i < h+1; i++){
+            for(int j = 0; j < v; j++){
+                if(size == 0){
+                    for(int k = 0; k <= ch; k++){
+                        mat[j][i] = hori.at(pri);
+                    }
+                    break;
+                }else{
                     mat[j][i] = hori.at(pri);
+                    pri++; 
+                    size--;
                 }
-                break;
-            }else{
-                mat[j][i] = hori.at(pri);
-                pri++; 
-                size--;
             }
-        }
-        pri--;
-        ch++;
-        i++;
-        for(int j = 0; j <= v; j++){
-            if(size == 0){
-                for(int k = 0; k <= ch; k++){
+            pri--;
+            ch++;
+            i++;
+            for(int j = 0; j <= v; j++){
+                if(size == 0){
+                    for(int k = 0; k <= ch; k++){
+                        mat[count][i] = hori.at(pri);
+                        
+                    }
+                    break;
+                }else{
                     mat[count][i] = hori.at(pri);
-                    
+                    pri++;
+                    count--; 
+                    size--;
                 }
-                break;
-            }else{
-                mat[count][i] = hori.at(pri);
-                pri++;
-                count--; 
-                size--;
+                
             }
-            
+            count = v;
         }
-        count = v;
-    }
 
-    
-    for(int i = 0; i < v; i++){
-        for(int j = 0; j < h; j++){
-            cout << mat[i][j] << " ";
+
+        
+        for(int i = 0; i < v; i++){
+            for(int j = 0; j < h; j++){
+                cout << mat[i][j] << " ";
+            }
+            cout<<endl;
         }
-        cout<<endl;
-    }
-    
-   
-	cout << endl;
+        reverse();
+        
+        hori.clear();
+        cout << endl;
 
+    }while(test == 0);
+    cout << endl << "Bye";
 	return 0;
     
 
